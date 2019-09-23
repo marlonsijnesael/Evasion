8cfd13931aa81e0ad3b21dd0908113be6e17e6b4
Marlon Sijnesael <m.a.sijnesael@gmail.com>
2019-09-23 15:10:26 +0200

    Added rotation

--- Rocketpower/Assets/Scripts/CameraController.cs
+++ Rocketpower/Assets/Scripts/CameraController.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float lookSensitivty = 1;
-    public float lookSmoothDamp = 1;
-    private float xRotation = 0f;
-    private float yRotation = 0f;
-    private float yRot = 0f;
-    private float yRotV, xRotV;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Transform cam;
+    [SerializeField] private float sensitivity = 5;
+    [SerializeField] private float maxLookUpDownAngle = 60; // can't look up/down more than 60 dgs
+    
+    private Vector3 pos;
+    private float upDownRotation = 0.0f;
+    private float rightLeftRotation = 0.0f;
+
 
     private void Update()
     {
-        var angH = Input.GetAxis("J1horizontal") * 60;
-        var angV = Input.GetAxis("rightstickvertical") * 45;
-        transform.localEulerAngles = new Vector3(angV, angH, 0);
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        Vector3 newRotation = new Vector3();
+        rightLeftRotation += InputCatcher.GetAxis("rightHorizontal", 1) * sensitivity * Time.deltaTime;
+
+        upDownRotation += InputCatcher.GetAxis("rightVertical", 1) * sensitivity * Time.deltaTime;
+        upDownRotation = Mathf.Clamp(upDownRotation, -maxLookUpDownAngle, maxLookUpDownAngle);
+        // getting the newRotation vector out of our left/right and up/down mouse values
+        // remember we rotate up/down around the x-axis and left/right around the y-axis
+        newRotation = new Vector3(upDownRotation, rightLeftRotation, 0);
+        transform.localRotation = Quaternion.Euler(newRotation);
     }
 }
+
--- Rocketpower/Assets/Scripts/PlayerClass.cs
+++ Rocketpower/Assets/Scripts/PlayerClass.cs
@@ -4,34 +4,30 @@ using UnityEngine;
 
 public class PlayerClass : MonoBehaviour
 {
-    
+
     [SerializeField] private int playerID;
     [SerializeField] private Camera playerCam;
     private float speed = 5;
     float moveHorizontal = 0f;
     private void Awake()
     {
-     //  Display.displays[playerID].Activate();
+        //  Display.displays[playerID].Activate();
     }
 
     private void Start()
     {
-       // playerCam.targetDisplay = playerID;
+        // playerCam.targetDisplay = playerID;
 
     }
 
     private void Update()
     {
-        moveHorizontal += InputCatcher.GetAxis("horizontal", playerID);
+        float moveHorizontal = InputCatcher.GetAxis("horizontal", playerID);
         float moveVertical = InputCatcher.GetAxis("vertical", playerID);
-        Vector3 newPosition = new Vector3 (moveHorizontal, 0.0f, moveVertical) + transform.forward;
-        Vector3 moveForward = transform.forward * moveVertical;
-        Vector3 rotateForward = moveForward;
-        rotateForward.x += moveHorizontal;
+        Vector3 pos =transform.position ;
+        pos.x += moveHorizontal;
+        pos.z += moveVertical;
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rotateForward), Time.deltaTime
-            );
-        transform.Translate(moveForward * speed * Time.deltaTime, Space.World);
-
+        transform.Translate(transform.forward* Time.deltaTime * moveVertical * speed, Space.World);
     }
 }
