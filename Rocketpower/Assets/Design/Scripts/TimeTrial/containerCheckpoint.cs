5a5c787ac91e145679d32865ce2a94fa0d45ab04
Chitinous <t.slootbeek@gmail.com>
2019-10-22 15:09:46 +0200

    Time Trial Functionality (Progress) + DegreeBendBig Fix

---
+++ Rocketpower/Assets/Design/Scripts/TimeTrial/containerCheckpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class containerCheckpoint : MonoBehaviour
+{
+    static private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        playerManager._Instance.UpdatecheckPoints(this.gameObject);
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Rocketpower/Assets/Design/Scripts/TimeTrial/startTimer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class startTimer : MonoBehaviour
+{
+    public static startTimer _Instance;
+
+    public GameObject TextUI;
+    public GameObject CheckpointUI;
+
+    public Text timerLabel;
+    public float time;
+
+    public bool isTimer;
+
+    private void Awake()
+    {
+        if (_Instance == null)
+        {
+            _Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Start")
+        {
+            time = 0;
+            isTimer = true;
+            TextUI.SetActive(true);
+            CheckpointUI.SetActive(true);
+            playerManager._Instance.ResetCheckpoints();
+            //other.gameObject.SetActive(false);
+        }
+
+        if (other.gameObject.tag == "Fin" && playerManager._Instance.checkPointsDict.Count == 0)
+        {
+            Debug.Log("True");
+            isTimer = false;
+            timerLabel.color = Color.green;
+        }
+    }
+
+    private void Update()
+    {
+        if (isTimer)
+        {
+            time += Time.deltaTime;
+
+            var minutes = time / 60;
+            var seconds = time % 60;
+            var fraction = (time * 100) % 100;
+
+            timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+            timerLabel.color = Color.white;
+        }
+    }
+
+}
