a67d49a6facc0da224a63a15eb00fc675063b62e
Marlon Sijnesael <m.a.sijnesael@gmail.com>
2019-10-24 21:12:27 +0200

    cool

--- Rocketpower/Assets/Scripts/Actual Movement/CCTest.cs
+++ Rocketpower/Assets/Scripts/Actual Movement/CCTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -20,30 +21,22 @@ public class CCTest : MonoBehaviour
     [Header("Scene objects: ")]
     [SerializeField] private Animator anim;
 
-    public Vector3 orthogonal = Vector3.zero;
+    public Vector3 wallrunDir = Vector3.zero;
     private Vector3 vel = Vector3.zero;
     private bool wallrun;
     private bool grounded;
     private RaycastHit verticalTest;
     public AnimContrller animationController;
-
-    public bool useGravityWhileRunning = false;
-
     private Vector3 verticalVel, horizontalVel;
     public float gravity = 14.0f;
     public float jumpVel = 14f;
-
-    [SerializeField] private float sensitivity = 5;
+    private bool slide;
+    public float slideForce = 10f;
 
     private void Awake()
     {
         accelRatePerSec = maxSpeed / timeZeroToMax;
         decelRatePerSec = -maxSpeed / timeMaxToZero;
-    }
-
-    private void Update()
-    {
-        RotateBody();
     }
 
     public void FixedUpdate()
@@ -53,29 +46,23 @@ public class CCTest : MonoBehaviour
         CheckGrounded();
         CheckInput();
         GroundTest();
-
+        SlideDown();
         Jump();
         LeftRightCollisions();
-        if (!wallrun)
+        if (!wallrun && !slide)
             Run();
         Reset();
-        Debug.DrawRay(transform.position, transform.rotation * Vector3.forward, Color.red);
-
     }
 
     private void Accelerate(float rate)
     {
         forwardVelocity += rate * Time.deltaTime;
         forwardVelocity = Mathf.Clamp(forwardVelocity, 0, maxSpeed);
-        // vel.z += forwardVelocity;
     }
 
     private void GroundTest()
     {
-        if (Physics.Raycast(transform.position, transform.up, out verticalTest))
-        {
-            Debug.DrawRay(transform.position, transform.up, Color.red);
-        }
+        Physics.Raycast(transform.position, -transform.up, out verticalTest);
     }
 
     private void CheckGrounded()
@@ -95,6 +82,15 @@ public class CCTest : MonoBehaviour
         {
             Accelerate(accelRatePerSec);
         }
+
+        if (Input.GetKey(KeyCode.X) && grounded)
+        {
+            slide = true;
+        }
+        else
+        {
+            slide = false;
+        }
     }
 
     private void LeftRightCollisions()
@@ -104,8 +100,8 @@ public class CCTest : MonoBehaviour
         {
             if (Vector3.Dot(hit.normal, Vector3.up) == 0)
             {
-                orthogonal = Vector3.Cross(hit.normal, Vector3.up);
-                Debug.DrawRay(hit.point, orthogonal, Color.red, 10f);
+                wallrunDir = Vector3.Cross(hit.normal, Vector3.up);
+                Debug.DrawRay(hit.point, wallrunDir, Color.red, 10f);
                 wallrun = true;
                 WallRun();
             }
@@ -116,19 +112,18 @@ public class CCTest : MonoBehaviour
             {
                 Debug.Log("left wall" + hit.transform.CompareTag("wall"));
 
-                orthogonal = Vector3.Cross(Vector3.up, hit.normal);
-                Debug.DrawRay(hit.point, orthogonal, Color.red, 10f);
+                wallrunDir = Vector3.Cross(Vector3.up, hit.normal);
+                Debug.DrawRay(hit.point, wallrunDir, Color.red, 10f);
                 wallrun = true;
                 WallRun();
             }
         }
         else
         {
-            orthogonal = Vector3.zero;
+            wallrunDir = Vector3.zero;
             wallrun = false;
         }
     }
-
 
     private void Jump()
     {
@@ -150,60 +145,25 @@ public class CCTest : MonoBehaviour
         Vector3 force = Vector3.zero;
         if (wallrun)
         {
-            if (!grounded && !Input.GetKey(KeyCode.X) && useGravityWhileRunning)
-            {
-                force.y = -5;
-
-            }
-            else if (!grounded && Input.GetKey(KeyCode.X))
-            {
-                force.y = 5;
-            }
-
-            vel += force + horizontalVel + (transform.rotation * Vector3.forward - orthogonal.normalized) * forwardVelocity;
-
-            animationController.SetBool(anim, AnimContrller.animations.running.ToString(), wallrun);
-            animationController.SetBool(anim, AnimContrller.animations.wallrun_right.ToString(), wallrun);
-
+            vel += force + horizontalVel + (transform.rotation * Vector3.forward - wallrunDir.normalized) * forwardVelocity;
             controller.Move((vel) * Time.deltaTime);
         }
+        animationController.SetBool(anim, AnimContrller.animations.wallrun_right.ToString(), wallrun);
+        animationController.SetBool(anim, AnimContrller.animations.running.ToString(), wallrun);
     }
-    float rightLeftRotation = 0f;
-    private void RotateBody()
+
+    private void SlideDown()
     {
-        // // Quaternion newRot = new Quaternion();
-        // rightLeftRotation = Input.GetAxisRaw("Horizontal") * sensitivity * Time.deltaTime;
-        // Vector3 rotation = new Vector3(0, rightLeftRotation, 0);
-        // // if (grounded)
-        // // {
-        // //     newRot = Quaternion.FromToRotation(transform.up, verticalTest.normal) * transform.rotation;
-        // // }
-
-        // // else
-        // // {
-        // //     newRot = Quaternion.FromToRotation(Vector3.zero, verticalTest.normal) * transform.rotation;
-
-        // // }
-
-        // // // newRot = Quaternion.Euler(rotation) * newRot;
-        // // transform.rotation = Quaternion.Slerp(transform.rotation, newRot, Time.deltaTime);
-        // Quaternion newRot = Quaternion.FromToRotation(transform.up, -verticalTest.normal) * transform.rotation * Quaternion.Euler(rotation);
-        // transform.rotation = Quaternion.Slerp(transform.rotation, newRot, Time.deltaTime * sensitivity);
-
-
-        float rotationSpeed = 25.0f;
-        if (Input.GetKey("escape"))
-            Application.Quit();
-
-        float rotation = Input.GetAxisRaw("Horizontal") * rotationSpeed;
-
-        rotation *= Time.deltaTime;
-
-        transform.Rotate(0, rotation, 0);
+        vel = Vector3.zero;
+        Vector3 force = Vector3.zero;
+        if (slide)
+        {
+            vel += (transform.rotation * Vector3.forward) * slideForce;
+            controller.Move((vel + verticalVel) * Time.deltaTime);
+        }
+        animationController.SetBool(anim, AnimContrller.animations.running.ToString(), slide);
+        animationController.SetBool(anim, AnimContrller.animations.sliding.ToString(), slide);
     }
-
-
-
 
     private Vector3 CalcForward
     {
@@ -211,33 +171,27 @@ public class CCTest : MonoBehaviour
         {
             if (grounded)
             {
-                return Vector3.Cross(verticalTest.normal, -transform.right);
+
+                Vector3 cross = Vector3.Cross(verticalTest.normal, -transform.right);
+                Debug.DrawRay(verticalTest.point, cross, Color.red, 10f);
+                return cross;
             }
             return transform.forward;
         }
     }
 
-    private void CalcMoveDir()
-    {
-
-    }
-
     private void Run()
     {
-
         if (!grounded && !wallrun)
         {
             verticalVel.y -= gravity * Time.deltaTime;
         }
         vel += (transform.rotation * Vector3.forward) * forwardVelocity;
-        // vel = transform.TransformDirection(vel);
         animationController.SetBool(anim, AnimContrller.animations.running.ToString(), vel.z != 0);
         animationController.SetBool(anim, AnimContrller.animations.wallrun_right.ToString(), false);
         animationController.SetBool(anim, AnimContrller.animations.wallrun_left.ToString(), false);
-        // Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical")); //+ transform.forward;
         controller.Move((vel + verticalVel) * Time.deltaTime);
     }
-
 
     private void Reset()
     {
@@ -250,6 +204,4 @@ public class CCTest : MonoBehaviour
             transform.rotation = startPoint.rotation;
         }
     }
-
-
 }
---
+++ Rocketpower/Assets/Scripts/Actual Movement/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public bool firstPerson = true;
+    public Transform firstPersonPos, thirdPersonPos;
+
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        // LockCursor();
+        xAxisClamp = 0.0f;
+
+        if (firstPerson)
+        {
+            transform.position = firstPersonPos.position;
+        }
+        else
+        {
+            transform.position = thirdPersonPos.position;
+        }
+    }
+
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        Debug.Log("Horizontal");
+        float mouseX = Input.GetAxis("Horizontal") * mouseSensitivity * Time.deltaTime;
+        float mouseY = 0;//Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
