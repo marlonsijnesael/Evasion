a49b9feac4056b23c535a7331b4cdc8f417b45fb
Chitinous <t.slootbeek@gmail.com>
2019-10-25 11:43:03 +0200

    Time Trial Functioneel
    - Random checkpoint order on start
    - Visible checkpoint + next checkpoint
    - Scalable checkpoint array size

--- Rocketpower/Assets/Design/Scripts/Player/playerManager.cs
+++ Rocketpower/Assets/Design/Scripts/Player/playerManager.cs
@@ -8,9 +8,10 @@ public class playerManager : MonoBehaviour
     public static playerManager _Instance;
 
     public Text checkpointText;
+
     private int checkpointIndex;
     private int checkpointNextIndex = 1;
-    private Vector3 checkpointHeight;
+
     private int waypointsAtStart;
     private int waypointsCleared;
 
@@ -21,16 +22,20 @@ public class playerManager : MonoBehaviour
     public GameObject[] checkpoints;
     public SortedDictionary<int, GameObject> checkPointsDict = new SortedDictionary<int, GameObject>();
 
+    private Vector3 checkpointHeight;
+    private Vector3 finishHeight;
+
     [SerializeField] private GameObject Finish;
-    public Material orangeCheckpoint;
-    public Material blueCheckpoint;
+    [SerializeField] private Material orangeCheckpoint;
+    [SerializeField] private Material blueCheckpoint;
+    [SerializeField] private Material nonactiveFinish;
+    [SerializeField] private Material activeFinish;
 
     public AudioClip hitEffect;
     private AudioSource audioSource;
 
     private void Awake()
     {
-
         checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
 
         if (_Instance == null)
@@ -47,24 +52,22 @@ public class playerManager : MonoBehaviour
     {
         waypointsAtStart = checkPointsDict.Count;
         audioSource = GetComponent<AudioSource>();
-        //ResetCheckpoints();
-
-        //Debug.Log(Finish);
     }
 
     private void Update() {
         NextCheckpointSequence();
-        Debug.Log("Checkpoint Next Index = " + checkpointNextIndex);
-        Debug.Log("Waypoints Cleared = " + waypointsCleared);
+        //Debug.Log("Checkpoint Next Index = " + checkpointNextIndex);
+        //Debug.Log("Waypoints Cleared = " + waypointsCleared);
     }
 
     public void ResetCheckpoints()
     {
+        checkpoints = ShuffleArray(checkpoints);
         Finish.SetActive(false);
         isFinishActive = false;
         checkpointNextIndex = 1;
         checkpointIndex = 0;
-        waypointsCleared = 1;
+        //waypointsCleared = 1;
 
         checkpointText.color = Color.white;
         checkpointText.text = 0 + " / " + checkpoints.Length;
@@ -78,9 +81,8 @@ public class playerManager : MonoBehaviour
 
         for (int i = 0; i < checkpoints.Length; i++)
         {
-            checkpoints = ShuffleArray(checkpoints);
             checkPointsDict.Add(i, checkpoints[i]);
-            checkPointsDict[0].SetActive(true);
+            //checkPointsDict[0].SetActive(true);
         }
     }
 
@@ -97,7 +99,6 @@ public class playerManager : MonoBehaviour
 
     public void UpdatecheckPoints(GameObject _checkpoint)
     {
-        //NextCheckpointSequence();
         audioSource.clip = hitEffect;
         audioSource.volume = 0.2f;
         audioSource.Play();
@@ -118,42 +119,42 @@ public class playerManager : MonoBehaviour
                     _checkpoint.SetActive(true);
                     checkPointsDict.Remove(i);
                     //float checkpointsCleared = waypointsCleared - checkPointsDict.Count;
-                    waypointsCleared++;
+                    //waypointsCleared++;
                 }
             }
-            //
         }
     }
 
-    public void NextCheckpointSequence()
-    {
-        for(int i = 0; i < checkpointIndex; i++) {
-            if (checkpointIndex == checkpoints.Length) {
-                Debug.Log("Finish Enabled");
-                Finish.SetActive(true);
-                checkpointText.color = Color.green;
-                isFinishActive = true;
-            }
-            else {
-                checkpoints[checkpointIndex].SetActive(true);
-                checkpoints[checkpointIndex].gameObject.GetComponent<Renderer>().material = blueCheckpoint;
-                checkpointHeight = new Vector3(60f, 50f, 60f);
-                checkpoints[checkpointIndex].gameObject.transform.localScale = checkpointHeight;
-                checkpoints[checkpointIndex].gameObject.GetComponent<Collider>().enabled = true;
-            }
+    public void NextCheckpointSequence() {
+        if(checkpointIndex == checkpoints.Length) {
+            isFinishActive = true;
+            checkpointText.color = Color.green;
+
+            Finish.gameObject.GetComponent<Renderer>().material = activeFinish;
+            finishHeight = new Vector3(60, 120, 60);
+            Finish.gameObject.transform.localScale = finishHeight;
+        }
+        else {
+            checkpoints[checkpointIndex].SetActive(true);
+            checkpoints[checkpointIndex].gameObject.GetComponent<Renderer>().material = blueCheckpoint;
+            checkpoints[checkpointIndex].gameObject.GetComponent<Collider>().enabled = true;
+            checkpointHeight = new Vector3(60, 80, 60);
+            checkpoints[checkpointIndex].gameObject.transform.localScale = checkpointHeight;
         }
 
-        for (int i = 0; i < checkpointNextIndex; i++) {
-            if (checkpointNextIndex >= checkpoints.Length) {
-                return;
-            }
-            else {
-                checkpoints[checkpointNextIndex].SetActive(true);
-                checkpoints[checkpointNextIndex].gameObject.GetComponent<Renderer>().material = orangeCheckpoint;
-                checkpoints[checkpointNextIndex].gameObject.GetComponent<Collider>().enabled = false;
-                checkpointHeight = new Vector3(60f, 25f, 60f);
-                checkpoints[checkpointNextIndex].gameObject.transform.localScale = checkpointHeight;
-            }
+        if (checkpointNextIndex < checkpoints.Length) {
+            checkpoints[checkpointNextIndex].SetActive(true);
+            checkpoints[checkpointNextIndex].gameObject.GetComponent<Renderer>().material = orangeCheckpoint;
+            checkpoints[checkpointNextIndex].gameObject.GetComponent<Collider>().enabled = false;
+            checkpointHeight = new Vector3(60, 40, 60);
+            checkpoints[checkpointNextIndex].gameObject.transform.localScale = checkpointHeight;
+        }
+        else if (checkpointIndex < checkpoints.Length){
+            Finish.SetActive(true);
+            Finish.gameObject.GetComponent<Renderer>().material = nonactiveFinish;
+            finishHeight = new Vector3(60, 40, 60);
+            Finish.gameObject.transform.localScale = finishHeight;
         }
     }
+
 }
