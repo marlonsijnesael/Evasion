045adc14e03a01b65df47ff18a7ded2a68b7a791
Marlon Sijnesael <m.a.sijnesael@gmail.com>
2019-10-28 10:20:30 +0100

    fixed camera rotation

---
+++ Rocketpower/Assets/CameraLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Horizontal") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Vertical") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up, mouseX);
+    }
+
+}
