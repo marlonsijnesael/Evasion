44acbed2f907c6cf30f55aaf78baa2a8c8336f47
HKU Student Account <Student@MF60.hku.local>
2019-11-19 17:04:26 +0100

    fixed the alpha blending of shader graph

---
+++ Rocketpower/Assets/Art Assets/Very Illegal (ArtitstScripts)/BecauseTheTimeNodeDoesntWork.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BecauseTheTimeNodeDoesntWork : MonoBehaviour
+{
+    
+	public List<Material> materialList;
+	
+	private void Update(){
+		foreach (Material m in materialList){
+			m.SetFloat("_t", Time.time);
+		}
+	}
+	
+}
