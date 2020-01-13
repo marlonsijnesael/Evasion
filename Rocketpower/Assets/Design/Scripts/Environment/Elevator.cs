735dabb4198c8e5103e6ff7ed51ebe04c9f67ed6
Chitinous <t.slootbeek@gmail.com>
2020-01-13 11:12:09 +0100

    Elevator Fix

--- Rocketpower/Assets/Design/Scripts/Environment/Elevator.cs
+++ Rocketpower/Assets/Design/Scripts/Environment/Elevator.cs
@@ -2,26 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Elevator : MonoBehaviour
 {
     public GameObject Hexagon;
+    public Transform startPoint, endPoint;
+    public float lerpTime = 3f;
 
-    private void OnTriggerStay(Collider other){
-        Hexagon.transform.position += Hexagon.transform.up * Time.deltaTime;
 
-    }
 
-    private void OnTriggerEnter(Collider other){
-        
-    }
 
-    void Start()
+
+    private IEnumerator MoveElevator(Transform player)
     {
-        
+        player.transform.SetParent(Hexagon.transform);
+        player.GetComponent<StateMachine>().enabled = false;
+        float dist = endPoint.position.y - startPoint.position.y;
+        float timeStep = dist / lerpTime;
+        float time = 0;
+
+        while (time < lerpTime)
+        {
+            Hexagon.transform.position = Vector3.Lerp(startPoint.position, endPoint.position, time / lerpTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        player.transform.SetParent(null);
+        player.GetComponent<StateMachine>().enabled = true;
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // other.transform.SetParent(this.transform);
+        StartCoroutine(MoveElevator(other.transform));
     }
 
     void Update()
     {
-        
+
     }
 }
