cc72b2881d61b4300e89e07dfebb0cf41360a82b
Melvin van Berkel <berkel.mvb@gmail.com>
2020-01-16 10:43:43 +0100

    UI tests
    added some prototypes for ui

---
+++ Rocketpower/Assets/Art Assets/Very Illegal/DirectionLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionLight : MonoBehaviour
+{
+public GameObject target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Vector3 targetPos = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+      transform.LookAt(targetPos);
+  
+    }
+}
---
+++ Rocketpower/Assets/Art Assets/Very Illegal/IndicatorLightRing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorLightRing : MonoBehaviour
+{
+ public GameObject player;
+ private float yPos;
+ private float xPos;
+ private float zPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+       xPos = gameObject.transform.position.x;
+       zPos = gameObject.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yPos = player.transform.position.y;
+
+        gameObject.transform.position = new Vector3(xPos,yPos,zPos);
+       
+    }
+}
