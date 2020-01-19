49a2cdd87ca67fe6616c34a9cde9a22b5436214c
Josien Vos <31687651+JosienVos@users.noreply.github.com>
2020-01-19 12:10:25 +0100

    changed some shaders and materials, cleaned up plebscenes

---
+++ Rocketpower/Assets/Art Assets/Very Illegal/ScreenCap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCap : MonoBehaviour {
+
+	public KeyCode screenCapKey = KeyCode.Return;
+	public int captureSize = 1;
+
+	// Use this for initialization
+	void Start() {
+		Debug.Log(Application.persistentDataPath);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(screenCapKey)) {
+			string date = System.DateTime.Now.ToString();
+			date = date.Replace("/", "-");
+			date = date.Replace(" ", "_");
+			date = date.Replace(":", "-");
+			ScreenCapture.CaptureScreenshot("ScreenCap_" + date + ".png", captureSize);
+			Debug.Log("screenshot captured, named ScreenCap_" + date + ".png");
+		}
+	}
+}
