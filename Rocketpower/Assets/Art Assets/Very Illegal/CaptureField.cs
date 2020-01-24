bda8bba8b320043c4e0f18783020248e6e041b14
Melvin van Berkel <berkel.mvb@gmail.com>
2020-01-24 11:49:13 +0100

    Capture fields added

---
+++ Rocketpower/Assets/Art Assets/Very Illegal/CaptureField.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureField : MonoBehaviour
+{
+    public Material mat;
+    public Transform otherPlayer;
+    public float minDist;
+    public float maxDist;
+    private float alphaSlide;
+    private float totalDist;
+    
+    void Start()
+    {
+        alphaSlide = mat.GetFloat("_Alpha");
+    }
+
+    
+    void Update()
+    {
+        if (otherPlayer)
+        {
+            totalDist = Vector3.Distance(otherPlayer.position, transform.position);
+            print(totalDist);
+        }
+        /*if (totalDist >= 5)
+        {
+            alphaSlide = Mathf.Clamp01();
+            mat.SetFloat("_Alpha", alphaSlide);
+        }
+        else
+        {
+            alphaSlide = 1f;
+             mat.SetFloat("_Alpha", alphaSlide);
+        }*/
+        alphaSlide = Mathf.Clamp01(-(maxDist - totalDist)/(minDist - maxDist));
+        mat.SetFloat("_Alpha", alphaSlide);
+    }
+}
